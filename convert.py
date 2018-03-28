740f7687a26930384b25030edb525244f889505a
Hako0 <34951444+Hako0@users.noreply.github.com>
2018-03-29 00:28:10 +0200

    Update convert.py

--- convert.py
+++ convert.py
@@ -1,4 +1,5 @@
 def ISBN13TO10(a) : 
+     #Strip the dashes
     a = a.replace("-", "")
     #If ISBN13 field already has the ISBN10 then return it
     if len(a) < 13 :
@@ -7,17 +8,21 @@ def ISBN13TO10(a) :
         a = a[3:12]
         b = list(a)
         sum = 0
-        #Do the math
+        #Do the math (first digit * 10 , second digit * 9 ... last digit * 2)
         for x in range(0, 9):
             b[x] = (10-x)* int(b[x])
+            #Take the sum of the new values
             sum +=int(b[x])
-
+        #The last digit in the ISBN10 will be 11 minus 11 mod the sum.
         lastDig = 11 - (sum % 11)
+        #If the last digit is 10 then concatenate the roman symbol for 10 - X 
         if lastDig == 10 :
             a = a + 'X'
+        #If the last digit is 11 then take concatenate 0
         elif lastDig == 11:
             lastDig = 0
             a = a + str(lastDig)
+        #Otherwise concatenate the last digit itself.
         else : 
             a = a + str(lastDig)
         return a
